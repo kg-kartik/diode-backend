b1d9df080de4f97266380c037a5d559246eb2a98
kg-kartik <goel.kartik39@gmail.com>
2022-06-27 03:04:52 +0530

    server init

---
+++ server.ts
@@ -0,0 +1,39 @@
+const bodyParser = require("body-parser");
+import express from "express";
+const app = express();
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose, { ConnectOptions } from "mongoose";
+import { ErrorHandler } from "./src/middlewares/errorHandler";
+import Users from "./src/routes/Users";
+
+app.use(bodyParser.json());
+app.use(cors());
+
+dotenv.config();
+
+const db: string =
+    "mongodb+srv://kartik:kartik@cluster0.sn1sx.mongodb.net/?retryWrites=true&w=majority";
+console.log(db);
+
+mongoose
+    .connect(db, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    } as ConnectOptions)
+    .then(() => {
+        console.log("databse ");
+    })
+    .catch((err) => {
+        console.log("Error connecting to the database", err);
+    });
+
+const PORTNUMBER: number = parseInt(process.env.PORT!) || 5000;
+
+app.use("/user", Users);
+
+app.use(ErrorHandler);
+
+app.listen(PORTNUMBER, (): void => {
+    console.log(`Server is running on ${PORTNUMBER}`);
+});
